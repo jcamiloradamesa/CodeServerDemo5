ec1da8c11a7a4d59549cd34d85ff94e6f1e69c7d
Hakan S. Kucuk <hakan.kucuk@ignitetech.com>
2019-10-08 00:19:01 +0300

    Update Main2.java

--- src/main/java/com/aurea/simple/Main2.java
+++ src/main/java/com/aurea/simple/Main2.java
@@ -8,7 +8,7 @@ public class Main2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World1");
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
         System.out.println("Hello World");
         Main main = new Main();
         main.test1();
